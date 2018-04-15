c2f775afa2877bbf6742dee997e2a413fb702ca0
BrunoCambianica <bruno.cambianica@gmail.com>
2018-04-15 20:28:56 +0200

    Resources
    Age minimum / verifications scope users Auth0

---
+++ Resources/MajorAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Resources
+{
+    public class MajorAttribute : ValidationAttribute
+    {
+        private int years;
+
+        public MajorAttribute(int years) : base()
+        {
+            this.years = years;
+        }
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime)
+            {
+                var dt = (DateTime)value;
+                if (dt.AddYears(years) <= DateTime.Now)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
